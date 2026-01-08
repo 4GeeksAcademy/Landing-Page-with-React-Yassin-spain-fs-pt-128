3b23eafee27714852bfd0fe50325ac5abbc921eb
winstylish <program.bussiness@gmail.com>
2026-01-08 21:03:48 +0100

    final

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 				</a>
 				<button
 					className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-					aria-controls="navbarNav" aria-expanded="false"aria-label="Toggle navigation"
+					aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
 				>
 					<span className="navbar-toggler-icon"></span>
 				</button>
