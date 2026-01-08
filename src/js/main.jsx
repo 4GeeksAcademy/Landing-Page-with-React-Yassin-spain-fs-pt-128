d427058520cd4ffd77097f2a509dbc39246a4ab0
winstylish <program.bussiness@gmail.com>
2026-01-08 20:16:50 +0100

    finest

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,54 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+
+const cardContent = {
+	image: "https://placehold.co/500x325?text=500x325",
+	title: "Card title",
+	text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus nequeum voluptatum laboriosam nihil, molestiae perferendis.",
+	buttonText: "Find Out More!",
+};
+
+const cards = [1, 2, 3, 4].map((id) => ({
+	id,
+	...cardContent,
+}));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+ <React.StrictMode>
+
+		<div>
+
+			<Navbar/>
+
+			<main className="container">
+
+				<Jumbotron/>
+
+				<div className="row g-4">
+
+					{cards.map((card) => (
+						<div className="col-12 col-sm-6 col-lg-3" key={card.id}>
+							<Card
+								image={card.image}
+								title={card.title}
+								text={card.text}
+								buttonText={card.buttonText}
+							/>
+						</div>
+					))}
+
+				</div>
+
+			</main>
+
+			<Footer/>
+
+		</div>
+
+	</React.StrictMode>
+);
